13adff2d8a63114fff9e99ce11a665d28a066761
Omar Ellaban <omar.ellaban00@gmail.com>
2024-08-22 22:23:52 +0300

    .

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Query
+from json import dump, load
+
+with open('info.json', 'w') as data:
+    dump(
+        {'msname' : '',
+         'stdnum' : 0,
+         'sessions' : {
+                        '1': {'attendance' : ['2', '40']}
+                    }
+         },
+        data
+    )
+    data.close()
+
+app=FastAPI()
+app.post('/attend/{data}')
+def attend(data : str):
+    splitted_data = data.split('|')
+    code = splitted_data[0]
+    sscode = splitted_data[1]
+    with open('info.json', 'w') as data:
+        js = load(data)
+        js['sessions'][sscode] = {'attendance' : []}
+        js['sessions'][sscode]['attendance'].append(code)
+        dump(js, dat)
+
+app.get('/sa')
+def sa():
+    return load(open('info.json', 'w'))
